ee5265ff7f1bf8a47687c1077e986472511a3d0f
belen-mapplas <belen@mapplas.com>
2013-04-23 08:58:13 +0200

    Database created with mapplas_postgis name. Added into settings file.

--- mapplas_server/mapplas_server/settings.py
+++ mapplas_server/mapplas_server/settings.py
@@ -11,9 +11,9 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'mapplas_postgis',
+
         'USER': '',
         'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
