4d02a742b017721a6c16f8dd3e166d660669ace8
AWS server Python 2.7 <belen@mapplas.com>
2013-07-08 08:12:30 +0000

    Created UsaCounties model

--- mapplas_server/usa_multipolygons/models.py
+++ mapplas_server/usa_multipolygons/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+class UsaCounties(models.Model):
+    # Regular Django fields corresponding to the attributes in the
+    # world borders shapefile.
+    county = models.CharField(max_length=80)
+    name = models.CharField(max_length=80, null=True)
+
+    # GeoDjango-specific: a geometry field (MultiPolygonField), and
+    # overriding the default manager with a GeoManager instance.
+    mpoly = models.MultiPolygonField()
+    
+    objects = models.GeoManager()
+
+    # Returns the string representation of the model.
+    def __unicode__(self):
+        return self.name + ' ' + self.county
